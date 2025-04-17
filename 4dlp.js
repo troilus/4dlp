bda8af59611d9ba71b5fe11327838699dc619463
troilus <iswangli@outlook.com>
2025-04-17 16:31:36 +0800

    Update 4dlp.js

--- 4dlp.js
+++ 4dlp.js
@@ -1,13 +1,18 @@
-const version = 'V1.0.0';
+const version = 'V1.0.1';
 
-// 解析响应内容
 let obj = JSON.parse($response.body);
 
-if (obj?.Response?.body?.["online-preview"]?.["online-preview-switch"]) {
-    obj.Response.body["online-preview"]["online-preview-switch"] = "0";
-    console.log(`[${version}] online-preview-switch has been set to "0"`);
-} else {
-    console.log(`[${version}] No online-preview-switch found or already "0"`);
+const previewSwitchPath = obj?.Response?.body?.["online-preview"];
+const watermarkPath = obj?.Response?.body?.["water-mark"];
+
+if (previewSwitchPath?.["online-preview-switch"] === "1") {
+    previewSwitchPath["online-preview-switch"] = "0";
+    console.log(`[${version}] online-preview-switch changed to "0"`);
+}
+
+if (watermarkPath?.["water-mark-switch"] === "1") {
+    watermarkPath["water-mark-switch"] = "0";
+    console.log(`[${version}] water-mark-switch changed to "0"`);
 }
 
 $done({ body: JSON.stringify(obj) });
