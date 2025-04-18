6ceeed0f30ef1b66da1739272e7bb7585a7366c0
troilus <iswangli@outlook.com>
2025-04-18 12:49:55 +0800

    Update 4dlp.js

--- 4dlp.js
+++ 4dlp.js
@@ -7,12 +7,15 @@ const watermarkPath = obj?.Response?.body?.["water-mark"];
 
 if (previewSwitchPath?.["online-preview-switch"] === "1") {
     previewSwitchPath["online-preview-switch"] = "0";
-    console.log(`[${version}] online-preview-switch changed to "0"`);
 }
 
 if (watermarkPath?.["water-mark-switch"] === "1") {
     watermarkPath["water-mark-switch"] = "0";
-    console.log(`[${version}] water-mark-switch changed to "0"`);
 }
+
+if (watermarkPath?.["water-mark-switch-text"] === "1") {
+    watermarkPath["water-mark-switch-text"] = "0";
+}
+
 
 $done({ body: JSON.stringify(obj) });
