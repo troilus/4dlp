844331fe5a11e65c8ec7595ba29cb66cd3ffa2d9
troilus <iswangli@outlook.com>
2025-04-27 14:57:29 +0800

    Update 4dlp.js

--- 4dlp.js
+++ 4dlp.js
@@ -4,24 +4,9 @@ let obj = JSON.parse($response.body);
 
 const previewSwitchPath = obj?.Response?.body?.["online-preview"];
 const watermarkPath = obj?.Response?.body?.["water-mark"];
-const filetranslimit = obj?.Response?.body?.["file-transfer-size-limit"];
 
 if (previewSwitchPath?.["online-preview-switch"] === "1") {
     previewSwitchPath["online-preview-switch"] = "0";
 }
-
-if (watermarkPath?.["water-mark-switch"] === "1") {
-    watermarkPath["water-mark-switch"] = "0";
-}
-
-if (watermarkPath?.["water-mark-switch-text"] === "1") {
-    watermarkPath["water-mark-switch-text"] = "0";
-}
-
-if (filetranslimit?.["file-transfer-pc-size-limit"] === "2048") {
-    filetranslimit["file-transfer-pc-size-limit"] = "204800";
-    filetranslimit["file-transfer-mb-size-limit"] = "204800";
-}
-
 
 $done({ body: JSON.stringify(obj) });
